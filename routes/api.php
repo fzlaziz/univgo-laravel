d186a7286cef8503fd84061e077151f826ff55d5
fzlaziz <84029684+fzlaziz@users.noreply.github.com>
2024-10-22 15:54:22 +0700

    feat: create simple api for news and campus

--- routes/api.php
+++ routes/api.php
@@ -10,3 +10,29 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::get('/news', function () {
+    return News::all();
+});
+
+Route::get('/news/{id}', function ($id) {
+    $newsDetail = News::with(['news_comments', 'campus'])->findOrFail($id);
+    $relatedNews = News::where('campus_id', $newsDetail->campus_id)
+                        ->where('id', '!=', $id)
+                        ->get();
+
+    return response()->json([
+        'news_detail' => $newsDetail,
+        'related_news' => $relatedNews,
+    ]);
+});
+
+
+Route::get('/campuses', function () {
+    return Campus::with([
+        'accreditation',
+        'study_programs.faculty',
+        'faculties',
+        'degree_levels',
+        'admission_routes',
+    ])->get();    
+});
